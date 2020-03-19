2384781cf73cd15d18c0c6b5408ff590166c985e
durbanro <48359550+durbanro@users.noreply.github.com>
2020-03-19 11:19:22 +0100

    Double Hashings
    1) Cada nueva entrada busca una posición libre
    2) Cuando una nueva entrada ve que una posicion esta ocupada swapea por el elemento que ya estaba colocado y se busca una nueva posicion con la segunda funcion

---
+++ DoubleHashing2.cpp
@@ -0,0 +1,88 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <utility>
+using namespace std;
+
+vector<int> hashTable;
+int occuped_space;
+int size;
+int colitions;
+
+//Function that initializate the hash table to the desired size
+void initHashTable(){
+	hashTable.resize(size, -1);
+}
+
+//Function that prints the whole hash table
+void print(){
+	cout << "Hash Table:" << endl;
+	for(int k = 0; k<size; k++){
+		cout << "[ " << k << " ] : " << hashTable[k] << endl;
+	}
+}
+
+//First Hash function
+int hash_f1(int key){ // Hash function 1
+	return (2*key+3)%size;
+}
+
+//Second Hash function
+int hash_f2(int key, int j){ // Hash function 2
+	return (j*(key))%size;
+}
+
+//Function that inserts a key into the hash table using double hashing
+bool insert(int key){
+	if(occuped_space != size){
+		int valuef1 = hash_f1(key); // We calculate position given by 1st Hash Function
+		if(hashTable[valuef1] != -1){	// Hash position not empty
+			int oldkey = hashTable[valuef1];
+			hashTable[valuef1] = key;	// We swap the old key in that position for a new one, and search a new pos for the old one.
+			colitions++;
+			int j = 0;
+			int valuef2 = hash_f2(oldkey, j); // We calculate position given by 2nd Hash Function
+			if(hashTable[valuef2] == oldkey) return true;
+			while(hashTable[valuef2] != -1){
+				colitions++;
+				j++;
+				valuef2 = hash_f2(oldkey, j);
+				cout << "que pasa; " << j << endl;
+			}
+			//No colition function 2
+			if(hashTable[valuef2] == oldkey) return true;
+			hashTable[valuef2] = oldkey;
+			occuped_space++;
+			return true;
+		}
+		else{ // No colition function 1
+			if(hashTable[valuef1] == key) return true;
+			hashTable[valuef1] = key;
+			occuped_space++;
+			return true;
+		}
+		if(hashTable[valuef1] == key) return true;
+	}
+	return false;
+}
+
+int main(){
+	cout << "Insert Hash size: " << endl;
+	cin >> size;
+	occuped_space = 0;
+	colitions = 0;
+
+	initHashTable();
+
+	cout << "Insert Hash values to insert: " << endl;
+	int key;
+	while(cin >> key){
+		bool result = insert(key);
+		if(!result) break;
+		print();
+		cout << "insert next:" << endl;
+	}
+
+	print();
+	cout << "Number of Insert Colitions = " << colitions << endl;
+}
