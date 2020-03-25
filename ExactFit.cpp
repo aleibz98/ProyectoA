869263e50171c016ee9480ed2b88d9cf7a056a7d
Edgarpb <edpbaz@gmail.com>
2020-03-25 19:31:43 +0100

    Add files via upload

--- ExactFit.cpp
+++ ExactFit.cpp
@@ -109,14 +109,14 @@ int main(){
   double rocupacion = (double) tocupacion / (double)Tmida;
   
   cout << "Nombre: ExactFit"<< endl;
-  cout << "Colisions Insert:" << cinsert << endl;
-  cout << "Colisions Search:" << csearch1+csearch2 << endl;
-  cout << "Colisions Totals:" << (double)cinsert+csearch1+csearch2 << endl;
-  cout << "Total Insert:" << tinsert << endl;
-  cout << "Total Comandes:" << tinsert+tsearch1+tsearch2  << endl;
-  cout << "Ratio de ocupacion contenedores:" << rocupacion << endl;
-  cout << "Media prueba teórica insertados:" << csearch1/(double)tsearch1 << endl;
-  cout << "Media prueba teórica no insertados:" << csearch2/(double)tsearch2 << endl;
+  cout << "Colisions Insert: " << cinsert << endl;
+  cout << "Colisions Search: " << csearch1+csearch2 << endl;
+  cout << "Colisions Totals: " << (double)cinsert+csearch1+csearch2 << endl;
+  cout << "Total Insert: " << tinsert << endl;
+  cout << "Total Comandes: " << tinsert+tsearch1+tsearch2  << endl;
+  cout << "Ratio de ocupacion contenedores: " << rocupacion << endl;
+  cout << "Media prueba teórica insertados: " << csearch1/(double)tsearch1 << endl;
+  cout << "Media prueba teórica no insertados: " << csearch2/(double)tsearch2 << endl;
   
  
   
